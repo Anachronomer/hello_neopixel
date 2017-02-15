ac387dc4bcf07740f1d737047cfd469653b8db8f
Mike Lustig <mlustig@covermymeds.com>
2017-02-15 16:05:03 -0500

    Basic GenServer thingy working

--- lib/effects.ex
+++ lib/effects.ex
@@ -17,28 +17,37 @@ defmodule HelloNeopixel.Effects do
   def off do {0, 0, 0} end
   def white do {255, 255, 255} end
 
-  def init(num_px) do
-    all(num_px, off(), 0)
-    {:ok, {nil, num_px}}
+  def blink(pid, color_1, color_2, brightness_1, brightness_2, delay) do
+    GenServer.cast(pid, {:blink, {color_1, color_2, brightness_1, brightness_2, delay}})
   end
 
-  def handle_cast({:start_blink, {color_1, color_2, brightness_1, brightness_2, delay}}, {animation_pid, num_px}) do
-    if animation_pid do
-      Process.exit(animation_pid, :kill)
+  def stop(pid) do
+    GenServer.call(pid, :stop)
+  end
+
+  def start_link(num_px) do
+    GenServer.start_link(__MODULE__, num_px, [name: __MODULE__])
+  end
+
+  def init(num_px) do
+    {:ok, %{anim_pid: nil, num_px: num_px}}
+  end
+
+  def handle_cast({:blink, {color_1, color_2, brightness_1, brightness_2, delay}}, state) do
+    if state.anim_pid do
+      Process.exit(state.anim_pid, :kill)
     end
 
-    animation_pid = spawn fn -> blink(num_px, color_1, color_2, brightness_1, brightness_2, delay) end
-    {:noreply, {animation_pid, num_px}}
+    anim_pid = spawn fn -> eternal_blink(state.num_px, color_1, color_2, brightness_1, brightness_2, delay) end
+    {:noreply, %{anim_pid: anim_pid, num_px: state.num_px}}
   end
 
-  def handle_cast(:stop, {animation_pid, num_px}) do
-    Process.exit(animation_pid, :kill)
-    {:noreply, {nil, num_px}}
+  def handle_call(:stop, state) do
+    Process.exit(state.anim_pid, :kill)
+    all(state.num_px, off(), 0)
+    {:ok, %{anim_pid: nil, num_px: state.num_px}}
   end
 
-  @doc """
-  Turns num_px pixels the given color and brightness.
-  """
   defp all(num_px, color, brightness) do
     # TODO: guard 0 < brightness < 255 ?
     # and color is a 3-tuple with all three values 0 < value < 255 ?
@@ -46,16 +55,10 @@ defmodule HelloNeopixel.Effects do
     Neopixel.render(@channel, {brightness, values})
   end
 
-  @doc """
-  Switches all pixels between one color/brightness and another with a given delay between each.
-  This will run indefinitely, so keep a handle of its pid to kill it.
-  TODO: How do I do a termination callback or signal without just ganking the process?
-  Or is process ganking the "right way"(tm)?
-  """
-  defp blink(num_px, color_1, color_2, brightness_1, brightness_2, delay) do
+  defp eternal_blink(num_px, color_1, color_2, brightness_1, brightness_2, delay) do
     # TODO: Guards?
     all(num_px, color_1, brightness_1)
     :timer.sleep(delay)
-    blink(num_px, color_2, color_1, brightness_2, brightness_1, delay)
+    eternal_blink(num_px, color_2, color_1, brightness_2, brightness_1, delay)
   end
 end
--- lib/hello_neopixel.ex
+++ lib/hello_neopixel.ex
@@ -4,13 +4,15 @@ defmodule HelloNeopixel do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    alias HelloNeopixel.Effects
+
     neopixel_cfg = Application.get_env(:hello_neopixel, :channel0)
     wifi_cfg = Application.get_env(:hello_neopixel, :wlan0)
 
     children = [
       worker(Nerves.Neopixel, [neopixel_cfg, nil]),
-      worker(HelloNeopixel.Effects, 60),
-      worker(Task, [fn -> Nerves.InterimWiFi.setup(:wlan0, wifi_cfg) end], restart: :transient, id: Init.Wifi)
+      #worker(Effects, [60]),
+      #worker(Task, [fn -> Nerves.InterimWiFi.setup(:wlan0, wifi_cfg) end], restart: :transient, id: Init.Wifi)
     ]
 
     opts = [strategy: :one_for_one, name: HelloNeopixel.Supervisor]
